ae5ab37090a974ecc6e074394cd6c99fc0787307
Arukwe Obo-Eba <oboebafriday@gmail.com>
2024-01-30 05:28:28 +0000

    fix broken code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,25 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+function processOptions(form) {
+    let optArray = [];
+
+    for (let e of form.entries()) {
+        if (e[0] === "options") {
+            optArray.push(e[1]);
+        }
+    }
+
+    form.delete("options");
+
+    form.append("options", optArray.join());
+
+    return form;
+}
+
 async function postForm(e) {
 
-    const form = new FormData(document.getElementById("checksform"));
+    const form = processOptions(new FormData(document.getElementById("checksform")));
 
     const response = await fetch(API_URL, {
         method: "POST",
@@ -22,6 +38,7 @@ async function postForm(e) {
     if (response.ok) {
         displayErrors(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 
@@ -36,12 +53,25 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        displayStatus(data.expiry); 
-        // console.log data will show {expiry: '28-01-2025', status_code: 200} while console.log data.expiry: 28-01-2025
+        displayStatus(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 
+}
+
+function displayException(data) {
+
+    let heading = `An Exception Occurred`;
+
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
 }
 
 function displayErrors(data) {
